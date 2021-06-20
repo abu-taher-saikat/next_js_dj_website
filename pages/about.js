a675fabca7a10a15868d082d4a7dc1644bd99c24
saikat1095 <mdabutahersaikat@gmail.com>
2021-06-21 02:18:58 +0600

    next installed and routes added

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is an app to find the leatest DJ and music events</p>
+            <p>Version: 1.0.0</p>
+        </div>
+    )
+}
+
+export default AboutPage
---
+++ pages/events/add.js
@@ -0,0 +1,7 @@
+export default function AddPage() {
+    return (
+        <div>
+            <h1>Add event page</h1>
+        </div>
+    )
+}
