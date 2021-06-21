bbcd2e87639e0b71c0113e1e038beb9933420f11
saikat1095 <mdabuthaersaikat@gmail.com>
2021-06-21 17:57:48 +0600

    getProps and getSaticProps added

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,39 @@
-import Layout from "@/components/Layout"
+import Head from 'next/head';
+import { API_URL } from '@/config/index';
+import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
+import styles from '@/styles/Layout.module.css'
 
-export default function Eventpage() {
-    return (
-        <Layout>
-            <h1>My Event</h1>
-        </Layout>
-    )
+export default function EventsPage({events}) {
+  console.log(events);
+  return (
+    <Layout>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No Events to Show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+
+      ))}
+    </Layout>
+  )
 }
+
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json();
+
+  return {
+    props : {events},
+    revalidate: 1,
+  }
+}
+// export async function getServerSideProps() {
+//   const res = await fetch(`${API_URL}/api/events`)
+//   const events = await res.json();
+
+//   return {
+//     props : {events}
+//   }
+// }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,46 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import { API_URL } from '@/config/index';
 import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
 import styles from '@/styles/Layout.module.css'
 
-export default function Home() {
+export default function Home({events}) {
+  console.log(events);
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No Events to Show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+
+      ))}
+
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   )
 }
+
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json();
+
+  return {
+    props : {events : events.slice(0,3)},
+    revalidate: 1,
+  }   
+}
+// export async function getServerSideProps() {
+//   const res = await fetch(`${API_URL}/api/events`)
+//   const events = await res.json();
+
+//   return {
+//     props : {events}
+//   }
+// }
