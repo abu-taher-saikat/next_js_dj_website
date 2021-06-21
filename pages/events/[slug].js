ff62f0db3f3ce3a6362d35dbe1ce0d8212e97fcf
saikat1095 <mdabuthaersaikat@gmail.com>
2021-06-21 22:31:20 +0600

    Event page added

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,16 +1,52 @@
 import {useRouter} from 'next/router';
+import Link from 'next/dist/client/link';
+import Image from 'next/dist/client/image';
+import {FaPencilAlt, FaTimes} from 'react-icons/fa'
 import Layout from '@/components/Layout';
 import { API_URL } from '@/config/index';
+import styles from '@/styles/Event.module.css';
+
 
 export default function EventPage({evt}) {
-    const router = useRouter();
+
+    const deleteEvent = (e)=>{
+        console.log('delete');
+    }
       
-    // console.log(router);
     return (
         <Layout>
-            <h1>{evt.name}</h1>
-            <h3>{router.query.slug}</h3>
-            <button onClick={()=> router.push('/')}>back to home</button>
+            <div className={styles.event}>
+                <div className={styles.controls}>
+                    <Link href={`/events/edit/${evt.id}`}>
+                        <a>
+                            <FaPencilAlt /> Edit Event
+                        </a>
+                    </Link>
+                    <a href="#" className={styles.delete} onClick={deleteEvent}>
+                        <FaTimes /> Delete Event
+                    </a>
+                </div>
+                <span>{evt.date} at {evt.time} </span>
+                <h1>{evt.name}</h1>
+                {evt.image && (
+                    <div className={styles.image}>
+                        <Image src={evt.image} width={960} height={600} alt="noting"></Image>
+                    </div>
+                )}
+
+                <h3>Performers:</h3>
+                <p>{evt.performers}</p>
+                <h3>Description:</h3>
+                <p>{evt.description}</p>
+                <h3>Venue : {evt.venue}</h3>
+                <p>{evt.address}</p>
+
+                <Link href="/events">
+                    <a className={styles.back}>
+                        {'<'} Go Back
+                    </a>
+                </Link>
+            </div>
         </Layout>
     )
 }
